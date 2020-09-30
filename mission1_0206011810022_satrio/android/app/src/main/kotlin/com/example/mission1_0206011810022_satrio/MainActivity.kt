ec17d493805cc4b2ffe69e542b1281bae9c7caf3
Xzellize90 <Xzellize90@gmail.com>
2020-09-30 20:08:07 +0800

    upload mission1

---
+++ mission1_0206011810022_satrio/android/app/src/main/kotlin/com/example/mission1_0206011810022_satrio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mission1_0206011810022_satrio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mission1_0206011810022_satrio/lib/main.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>with SingleTickerProviderStateMixin {
+
+  bool _hasBeenPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Mission 1"),
+        ),
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/background.jpg"),
+              fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          Column(
+            children: <Widget>[
+              Flexible(
+              flex: 10,
+              child: Container(
+                width: 500,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/main.png"),
+                      fit: BoxFit.fill
+                    )
+                  ),
+            )),Flexible(
+              flex: 5,
+              child: Container(
+                width: 412,
+                height: 250,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("images/image1.png"),
+                                fit: BoxFit.fill
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            margin: EdgeInsets.fromLTRB(7.5, 0, 5, 7.5),
+                          ),
+                          Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("images/image2.jpg"),
+                                fit: BoxFit.fill
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            margin: EdgeInsets.fromLTRB(7.5, 0, 5, 7.5),
+                          ),
+                          Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("images/image3.jpg"),
+                                fit: BoxFit.fill
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            margin: EdgeInsets.fromLTRB(7.5, 0, 5, 7.5),
+                          ),
+                          Container(
+                            width: 90,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("images/image4.jpg"),
+                                fit: BoxFit.fill
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            margin: EdgeInsets.fromLTRB(7.5, 0, 5, 7.5),
+                          ),
+                        ],
+                      ),
+            )),Flexible(
+              flex: 2,
+              child: Container(
+                width: 412,
+                  height:40,
+                  child: ListView(
+                    children: <Widget>[
+                      Text(
+                    "Cyberpunk 2077",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                    ],
+                  ),
+            )),
+            Flexible(
+              flex: 11,
+              child: ListView(
+                children: <Widget>[
+                  Text(
+                    "        Cyberpunk 2077 adalah game yang paling di tunggu tunggu oleh gamer di seluruh dunia, game ini bertema futuristik open world dengan fitur no loading, di tambah dengan kehadiran kehadiran keanu reeves membuan para ganer dan fans keanu reeves tidak sabar untuk memainkan game ini, game ini di rencakan akan keluar pada november 2020 setelah di delay sebanyak 3 kali, spec req game ini pun termasuk rendah di kelasnya, game ini sekrang sudah dapat di pre-order di steam, GOC, amazone, dan epic games",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    " ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    "        Cyberpunk 2077 mulai diumumkan pada tahun 2017, mulai dari 2017 hingga 2020 cyberpunk di develop dan cd project red (developer cyberpunk 2077 dan the witcher menemukan banyak eror di game tersebut sehingga, dilakukan revisi dan perbaikan hingga delay sebanyak 3 kali dari tanggal release yang sudah di tetapkan",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ],
+              ),
+              ),
+            ],
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            margin: EdgeInsets.only(top: 12, right: 10),
+            child: FloatingActionButton(
+                child: Icon(Icons.favorite,
+                  color: _hasBeenPressed? Colors.red : Colors.grey,
+                ),
+                backgroundColor: Colors.white,
+                onPressed: () {setState(() {
+                  _hasBeenPressed = !_hasBeenPressed;
+                });}),
+            ),
+          ],
+          ),
+          
+        ),
+      ),
+    );
+  }
+  List<Color> _colors = <Color>[
+    Colors.blue
+  ];
+
+}
+
+
