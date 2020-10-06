d781dba78b71dbcc5be4ca7afc6523720da86138
Xzellize90 <Xzellize90@gmail.com>
2020-10-07 00:15:07 +0800

    this is the mission2 of TEPD
    popup, pindah halaman dan text field

---
+++ mission1_0206011810022_satrio/lib/preorder.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:mission1_0206011810022_satrio/home.dart';
+
+class po extends StatefulWidget {
+
+  @override
+  _poState createState() => _poState();
+}
+
+class _poState extends State<po> {
+
+  var nama;
+  var email;
+  var nicname;
+  var alamat;
+
+  final namaCon = new TextEditingController();
+  final emailCon = new TextEditingController();
+  final nicnameCon = new TextEditingController();
+  final alamatCon = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pre Order"),
+      ),
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+            Flexible(
+               flex: 3,
+                child:Container(
+                  margin: EdgeInsets.only(left:20, right:20, top:5),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: namaCon,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          suffix: Container(
+                          width: 5,
+                          height: 5,
+                          color: Colors.red,
+                          ),
+                            prefixIcon: Icon(Icons.person),
+                            prefixText: "Nama :",
+                            prefixStyle: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.w600
+                          ),
+                        labelText: "Nama Lengkap",
+                        hintStyle: TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                        ),
+                        ),
+                       ),
+                      ]
+                    ),
+                  ),
+                  ),
+            Flexible(
+               flex: 3,
+                child:Container(
+                  margin: EdgeInsets.only(left:20, right:20, top:5),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: emailCon,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          suffix: Container(
+                          width: 5,
+                          height: 5,
+                          color: Colors.red,
+                          ),
+                            prefixIcon: Icon(Icons.mail),
+                            prefixText: "Email :",
+                            prefixStyle: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.w600
+                          ),
+                        labelText: "Alamat Email",
+                        hintStyle: TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                        ),
+                        ),
+                       ),
+                      ]
+                    ),
+                  ),
+                  ),
+            Flexible(
+               flex: 3,
+                child:Container(
+                  margin: EdgeInsets.only(left:20, right:20, top:5),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: nicnameCon,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          suffix: Container(
+                          width: 5,
+                          height: 5,
+                          color: Colors.red,
+                          ),
+                            prefixIcon: Icon(Icons.person),
+                            prefixText: "NickName :",
+                            prefixStyle: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.w600
+                          ),
+                        labelText: "Steam Account",
+                        hintStyle: TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                        ),
+                        ),
+                       ),
+                      ]
+                    ),
+                  ),
+                  ),
+            Flexible(
+               flex: 3,
+                child:Container(
+                  margin: EdgeInsets.only(left:20, right:20, top:5),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: alamatCon,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          suffix: Container(
+                          width: 5,
+                          height: 5,
+                          color: Colors.red,
+                          ),
+                            prefixIcon: Icon(Icons.home),
+                            prefixText: "Alamat :",
+                            prefixStyle: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.w600
+                          ),
+                        labelText: "Alamat Lengkap",
+                        hintStyle: TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)
+                        ),
+                        ),
+                       ),
+                      ]
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 3,
+                  child: Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: RaisedButton(
+                      child: Text("Pre Order",style: TextStyle(color: Colors.grey)),
+                      color: Colors.yellowAccent,
+                      onPressed: (){
+                        setState(() {
+                          nama = namaCon.text;
+                          email = emailCon.text;
+                          nicname = nicnameCon.text;
+                          alamat = alamatCon.text;
+                        });
+
+                        if(nama != "" || email != "" || nicname != "" || alamat != ""){
+                          return showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Dialog(
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  overflow: Overflow.visible,
+                                  children: <Widget>[
+                                     Container(
+                                  height: 230,
+                                  width: 190,
+                                    child: Column(
+                                      children: <Widget>[
+                                        SizedBox(height: 20,),
+                                        new Text("PO Success", textAlign: TextAlign.center, style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+                                        new Text("Selamat $nama",style: TextStyle(fontSize: 14), textAlign: TextAlign.left,),
+                                        new Text("Email: $email",style: TextStyle(fontSize: 14), textAlign: TextAlign.left,),
+                                        new Text("NickName Steam: $nicname",style: TextStyle(fontSize: 14), textAlign: TextAlign.left,),
+                                        new Text("Alamat: $alamat",style: TextStyle(fontSize: 14), textAlign: TextAlign.left,),
+                                        Container(
+                                          alignment: Alignment.bottomCenter,
+                                          child: FlatButton(
+                                              onPressed: () {
+                                                Navigator.push(context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) {
+                                                  return Home();
+                                                }));
+                                              },  
+                                              child: Text("Next",style: TextStyle(color: Colors.green),)),
+                                        )
+                                      ],
+                                    ),
+                                  
+                                )
+                                  ]
+                                ),
+                              );
+                            }
+                          );
+                        }
+                        return showDialog(
+                            context: context,
+                            builder: (context) {
+                              return Dialog(
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  overflow: Overflow.visible,
+                                  children: <Widget>[
+                                     Container(
+                                  height: 230,
+                                  width: 190,
+                                    child: Column(
+                                      children: <Widget>[
+                                        SizedBox(height: 20,),
+                                        new Text("Eror!", textAlign: TextAlign.center, style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,color: Colors.yellow),),
+                                        new Text("All Field Must Be Filled",style: TextStyle(fontSize: 14,color: Colors.red), textAlign: TextAlign.left,),
+                                        Container(
+                                          alignment: Alignment.bottomCenter,
+                                          child: FlatButton(
+                                              onPressed: () {
+                                                Navigator.pop(context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) {
+                                                  return Home();
+                                                }));
+                                              },  
+                                              child: Text("Close",style: TextStyle(color: Colors.green),)),
+                                        )
+                                      ],
+                                    ),
+                                  
+                                )
+                                  ]
+                                ),
+                              );
+                            }
+                          );
+                      }
+                    ),
+                  )
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
